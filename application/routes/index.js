90363963870d493cd5839e251d9c8533eee8462c
zacr0 <zacro.suarez@gmail.com>
2014-05-17 20:25:00 +0200

    Add views

--- application/routes/index.js
+++ application/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'SocialGcap - Inicio' });
 });
 
 module.exports = router;
