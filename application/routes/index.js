9d7e89e1cc1f45cd3a9dc49a68f291370e0a7158
zacr0 <zacro.suarez@gmail.com>
2014-05-19 00:40:18 +0200

    Modifica vistas, actualiza rutas

--- application/routes/index.js
+++ application/routes/index.js
@@ -1,14 +1,14 @@
 var route = function (app) {
 	app.get('/', function(req, res) {
-		res.render('index');
+		res.render('index', { title: 'SocialGcap - Inicio' });
 	});
 
 	app.get('/login', function(req, res) {
-		res.render('login');
+		res.render('login', { title: 'SocialGcap - Iniciar sesión'});
 	});
 
 	app.get('/registro', function(req, res) {
-		res.render('registro');
+		res.render('registro', { title: 'SocialGcap - Registro'});
 	});
 
 	/* AQUI FALTA EL APARTADO POST DEL REGISTRO */
