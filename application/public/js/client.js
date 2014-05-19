2c06d08ff57b9187baaef973e7ccc3890a4cbeff
zacr0 <zacro.suarez@gmail.com>
2014-05-20 00:25:08 +0200

    Add validacion

--- application/public/js/client.js
+++ application/public/js/client.js
@@ -1,5 +1,20 @@
 $(function() {
-	$('#fecha-nacimiento').datepicker({
+	// PAGINA DE LOGIN
+	var loginForm = $('#form-login');
+
+	$(loginForm).validate({
+		rules: {
+			usuario: {
+				required: true
+			},
+			pass: {
+				required: true
+			}
+		}
+	});
+
+	// PAGINA DE REGISTRO
+	$('#fechanacimiento').datepicker({
 		format: 'dd/mm/yyyy',
 		endDate: new Date($.now()),
 		startDate: new Date('01/01/1970'),
@@ -15,11 +30,74 @@ $(function() {
 	$('input[name=perfil]').click(function () {
 		if (this.id == "alumno") {
 			$(".visible-alumno").show();
+			$("#alumno").attr('required') = 
 			$(".visible-profesor").hide();
 		} 
 		if (this.id == "profesor") {
 			$(".visible-profesor").show();
 			$(".visible-alumno").hide();
 		}
+	});
+
+	var registroForm = $('#form-registro');
+	registroForm.validate({
+		rules: {
+			usuario: {
+				required: true,
+				rangelength: [3,12]
+			},
+			pass: {
+				required: true,
+				pwd: true,
+				minlength: 6
+			},
+			nombre: {
+				required: true,
+				nombre: true,
+				minlenght: 3
+			},
+			apellidos: {
+				required: true,
+				nombre: true,
+				minlength: 3
+			},
+			email: {
+				required: true,
+				email: true
+			},
+			fechanacimiento: {
+				date: true
+			}
+			// Validacion de promocion/curso/asignaturas:
+		},
+		messages: {
+			pass: {
+				pwd: "La contraseña debe tener al menos 6 caracteres, \
+					una minúscula, una mayúscula y un número."
+			},
+			nombre: {
+				nombre: "El nombre sólo puede contener letras."
+			},
+			apellidos: {
+				nombre: "Los apellidos sólo pueden contener letras."
+			}
+		}
+	});
+	/*
+		El nombre debe tener 3 o mas letras y puede ser compuesto
+	*/
+	$.validator.addMethod("nombre", function(value) {
+		return /^([a-zA-Z]{3,}\s*)+$/.test(value)
+	});
+
+	/*
+		La contraseña debe tener:
+		 - Al menos 6 caracteres
+		 - Una minuscula
+		 - Una mayuscula
+		 - Un numero
+	*/
+	$.validator.addMethod("pwd", function(value) {
+		return /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/.test(value);
 	});
 });
