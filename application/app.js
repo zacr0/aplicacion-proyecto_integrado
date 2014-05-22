32a38afbaf48ff9041465afe59379da1af9ea306
zacr0 <zacro.suarez@gmail.com>
2014-05-22 12:18:43 +0200

    Modify validacion, error, favicon
    Añadida validacion para el nombre de usuario en la pagina de registro,
    reemplazado el favicon de express por uno personalizado y modificados
    algunos de los mensajes de error

--- application/app.js
+++ application/app.js
@@ -26,7 +26,7 @@ admin_routes(app);
 
 /// catch 404 and forward to error handler
 app.use(function(req, res, next) {
-    var err = new Error('Not Found');
+    var err = new Error('No encontrado');
     err.status = 404;
     next(err);
 });
