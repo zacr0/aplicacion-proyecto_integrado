88a244a4cde6d9d28be210de2aa65ca52df5ed02
rrodriguez <rrodriguez@socialbro.com>
2014-05-24 00:34:13 +0200

    Perfil

--- application/routes/perfil.js
+++ application/routes/perfil.js
@@ -8,6 +8,16 @@ var route = function (app) {
 				'para acceder a SocialGcap.'});
 		}
 	});
+
+	app.get('/perfil/:usuario', function(req, res) {
+		console.log('ESTAS EN TU USUARIO');
+		if (req.session.usuario) {
+			res.render('perfil', {usuario: req.session.usuario});
+		} else {
+			res.render('login', {error: 'Debes iniciar sesión ' +
+				'para acceder a SocialGcap.'});
+		}
+	});
 }
 
 module.exports = route;
