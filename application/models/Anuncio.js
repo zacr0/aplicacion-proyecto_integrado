953b4897a94fc3ea0ef9cae36b3d774e7be32293
rrodriguez <rrodriguez@socialbro.com>
2014-05-25 00:15:05 +0200

    Implementacion controlador usuarios

--- application/models/Anuncio.js
+++ application/models/Anuncio.js
@@ -1,14 +1,5 @@
 var mongoose = require('mongoose'), Schema = mongoose.Schema;
 
-mongoose.connect(connStr, function(err){
-  if (err) {
-    console.log('Error al conectar a MongoDB');
-    throw err;
-  } else {
-    console.log('Conectado a MongoDB');
-  }
-});
-
 var Anuncio = new Schema({
 	id_usuario: Schema.ObjectId,
 	titulo: {type: String, trim: true, required: true},
--- application/routes/usuarios.js
+++ application/routes/usuarios.js
@@ -1,8 +1,67 @@
-var route = function (app) {
+var Usuario = require('../models/Usuario'),
+	Promocion = require('../models/Promocion'),
+
+route = function (app) {
 	app.get('/usuarios', function(req, res) {
 
 		if (req.session.usuario) {
-			res.render('usuarios', {usuario: req.session.usuario});
+			//res.render('usuarios', {usuario: req.session.usuario});
+			Usuario.find(function (err, user) {
+				console.log('usuarios: ' + user);
+				res.send('aaaaaaaa' + user);
+			})
+		} else {
+			res.render('login', {error: 'Debes iniciar sesión ' +
+				'para acceder a SocialGcap.'});
+		}
+
+	});
+
+	app.get('/usuarios/alumnos', function(req, res) {
+
+		if (req.session.usuario) {
+			Usuario.find(function (err, user) {
+				console.log('usuarios: ' + user);
+				res.send('aaaaaaaa' + user);
+			})
+		} else {
+			res.render('login', {error: 'Debes iniciar sesión ' +
+				'para acceder a SocialGcap.'});
+		}
+
+	});
+
+	app.get('/usuarios/profesores', function(req, res) {
+
+		if (req.session.usuario) {
+			Usuario.find({perfil: 'profesor'},function (err, user) {
+				console.log('usuarios: ' + user);
+				res.send('aaaaaaaa' + user);
+			});
+		} else {
+			res.render('login', {error: 'Debes iniciar sesión ' +
+				'para acceder a SocialGcap.'});
+		}
+
+	});
+
+	app.get('/usuarios/promociones', function(req, res) {
+
+		var queryPromocion = Promocion.find().sort({nombre: -1});
+
+		// **** Importante éste código
+		if (req.session.usuario) {
+			queryPromocion.exec( function (err, dataPromocion) {
+				dataPromocion.forEach(function (elem, index, array) {
+					console.log('elem: ' + elem
+						+ '\nindex: ' + index
+						+ '\narray: ' + array);
+					Usuario.find({perfil: 'alumno', id_promocion: elem.id}).exec(function (err, data) {
+						res.send('->' + dataPromocion +
+							+ '\n\n' + data);
+					});
+				});
+			});
 		} else {
 			res.render('login', {error: 'Debes iniciar sesión ' +
 				'para acceder a SocialGcap.'});
