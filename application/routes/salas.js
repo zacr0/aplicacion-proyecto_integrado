35c74e50a4e444e693c0ccba615efd335f51c651
rrodriguez <rrodriguez@socialbro.com>
2014-05-29 23:40:18 +0200

    Funciona el chat

--- application/routes/salas.js
+++ application/routes/salas.js
@@ -1,8 +1,21 @@
-var route = function (app) {
+var app = require('express')(),
+    server = require('http').Server(app),
+    io = require('socket.io')(server),
+
+route = function (app) {
 	app.get('/salas', function(req, res) {
 
 		if (req.session.usuario != undefined) {
+
+			io.sockets.on('connection', function (socket) {
+
+			  socket.on('message', function (message) {
+			  	io.sockets.emit('message', message);
+			  });
+			});
+
 			res.render('salas', {usuario: req.session.usuario});
+
 		} else {
 			res.render('login', {error: 'Debes iniciar sesión ' +
 				'para acceder a SocialGCap.'});
