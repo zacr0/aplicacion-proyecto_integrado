5e4e1a0c4591012ca4ba808b6c9ce00a8370ad32
zacr0 <zacro.suarez@gmail.com>
2014-06-05 10:29:11 +0200

    Cambia redireccion listado de usuarios

--- application/routes/usuarios.js
+++ application/routes/usuarios.js
@@ -3,145 +3,144 @@ var Usuario = require('../models/Usuario'),
 	async = require('async'),
 	url = require('url'),
 	query,
+	route = function (app) {
+		app.get('/usuarios', function(req, res) {
 
-route = function (app) {
-	app.get('/usuarios', function(req, res) {
+			if (req.session.usuario != undefined) {
+				query = Usuario.find({},{_id: 0, fechaNacimiento: 0, email: 0, pass: 0, asignaturasProfesor: 0})
+								.sort({nombre: 1, apellidos: 1});
 
-		if (req.session.usuario != undefined) {
-			query = Usuario.find({},{_id: 0, fechaNacimiento: 0, email: 0, pass: 0, asignaturasProfesor: 0})
-							.sort({nombre: 1, apellidos: 1});
-
-			query.exec(function (err, users) {
-				res.render('usuarios', {usuario: req.session.usuario,
-					usuarios: users,
-					ver: 'usuarios'
-				});
-			})
-		} else {
-			res.render('login', {error: 'Debes iniciar sesión ' +
-				'para acceder a SocialGCap.'});
-		}
-
-	}); // /usuarios
-
-	app.get('/usuarios/alumnos', function(req, res) {
-
-		if (req.session.usuario != undefined) {
-			query = Usuario.find({perfil: 'alumno'},{_id: 0, fechaNacimiento: 0, email: 0, pass: 0, asignaturasProfesor: 0})
-							.sort({nombre: 1, apellidos: 1});
-
-			query.exec(function (err, users) {
-				res.render('usuarios', {usuario: req.session.usuario,
-					usuarios: users,
-					ver: 'alumnos'
-				});
-			})
-		} else {
-			res.render('login', {error: 'Debes iniciar sesión ' +
-				'para acceder a SocialGCap.'});
-		}
-
-	}); // /usuarios/alumnos
-
-	app.get('/usuarios/profesores', function(req, res) {
-
-		if (req.session.usuario != undefined) {
-			query = Usuario.find({perfil: 'profesor'},{_id: 0, fechaNacimiento: 0, email: 0, pass: 0, asignaturasProfesor: 0})
-							.sort({nombre: 1, apellidos: 1});
-
-			query.exec(function (err, users) {
-				res.render('usuarios', {usuario: req.session.usuario,
-					usuarios: users,
-					ver: 'profesores'
-				});
-			});
-		} else {
-			res.render('login', {error: 'Debes iniciar sesión ' +
-				'para acceder a SocialGCap.'});
-		}
-
-	}); // /usuarios/profesores
-
-	app.get('/usuarios/promociones', function(req, res) {
-
-		if (req.session.usuario != undefined) {
-			query = Promocion.find({}, {_id: 0}).sort({nombre: -1});
-
-			query.exec( function (err, dataPromocion) {
-				if (err) {
-					return console.log(err);
-				} else {
-					console.log(dataPromocion);
+				query.exec(function (err, users) {
 					res.render('usuarios', {usuario: req.session.usuario,
-						promociones: dataPromocion,
-						ver: 'promociones'});
-				}
-			});
-		} else {
-			res.render('login', {error: 'Debes iniciar sesión ' +
-				'para acceder a SocialGCap.'});
-		}
+						usuarios: users,
+						ver: 'usuarios'
+					});
+				})
+			} else {
+				res.render('login', {error: 'Debes iniciar sesión ' +
+					'para acceder a SocialGCap.'});
+			}
 
-	}); // /usuarios/promociones
+		}); // /usuarios
 
-	app.get('/usuarios/promociones/:promocion', function(req, res) {
+		app.get('/usuarios/alumnos', function(req, res) {
 
-		if (req.session.usuario != undefined) {
-			query = Promocion.findOne({"nombre": req.params.promocion});
+			if (req.session.usuario != undefined) {
+				query = Usuario.find({perfil: 'alumno'},{_id: 0, fechaNacimiento: 0, email: 0, pass: 0, asignaturasProfesor: 0})
+								.sort({nombre: 1, apellidos: 1});
 
-			query.exec(function (err, dataPromocion) {
-				if(err){
-                  return console.log(err);
-				} else {
-					Usuario.find({"id_promocion": dataPromocion},
-									{_id: 0, fechaNacimiento: 0, email: 0, pass: 0, asignaturasProfesor: 0},
-									function (err, dataUsers) {
+				query.exec(function (err, users) {
+					res.render('usuarios', {usuario: req.session.usuario,
+						usuarios: users,
+						ver: 'alumnos'
+					});
+				})
+			} else {
+				res.render('login', {error: 'Debes iniciar sesión ' +
+					'para acceder a SocialGCap.'});
+			}
+
+		}); // /usuarios/alumnos
+
+		app.get('/usuarios/profesores', function(req, res) {
+
+			if (req.session.usuario != undefined) {
+				query = Usuario.find({perfil: 'profesor'},{_id: 0, fechaNacimiento: 0, email: 0, pass: 0, asignaturasProfesor: 0})
+								.sort({nombre: 1, apellidos: 1});
+
+				query.exec(function (err, users) {
+					res.render('usuarios', {usuario: req.session.usuario,
+						usuarios: users,
+						ver: 'profesores'
+					});
+				});
+			} else {
+				res.render('login', {error: 'Debes iniciar sesión ' +
+					'para acceder a SocialGCap.'});
+			}
+
+		}); // /usuarios/profesores
+
+		app.get('/usuarios/promociones', function(req, res) {
+
+			if (req.session.usuario != undefined) {
+				query = Promocion.find({}, {_id: 0}).sort({nombre: -1});
+
+				query.exec( function (err, dataPromocion) {
+					if (err) {
+						return console.log(err);
+					} else {
+						console.log(dataPromocion);
 						res.render('usuarios', {usuario: req.session.usuario,
 							promociones: dataPromocion,
-							usuarios: dataUsers,
-							ver: req.params.promocion});
-					});
-				}
-			});
-		} else {
-			res.render('login', {error: 'Debes iniciar sesión ' +
-				'para acceder a SocialGCap.'});
-		}
+							ver: 'promociones'});
+					}
+				});
+			} else {
+				res.render('login', {error: 'Debes iniciar sesión ' +
+					'para acceder a SocialGCap.'});
+			}
 
-	}); // /usuarios/promociones
+		}); // /usuarios/promociones
 
-	app.post('/usuarios/buscar', function (req, res) {
-		
-		if (req.session.usuario != undefined) {
-			query = Usuario.find({},{_id: 0, fechaNacimiento: 0, email: 0, pass: 0, asignaturasProfesor: 0})
-							.or([{'usuario': {$regex: new RegExp(req.body.buscador, "i")}}, 
-								{'nombre': {$regex: new RegExp(req.body.buscador, "i")}}, 
-								{'apellidos': { $regex: new RegExp(req.body.buscador, "i")}}])
-							.sort({nombre: 1, apellidos: 1});
+		app.get('/usuarios/promociones/:promocion', function(req, res) {
 
-			query.exec(function (err, users) {
-				if (err) {
-					return console.log(err);
-				}
+			if (req.session.usuario != undefined) {
+				query = Promocion.findOne({"nombre": req.params.promocion});
 
-				if (users) {
-					res.render('usuarios', {
-						usuario: req.session.usuario,
-						usuarios: users,
-						ver: 'usuarios encontrados'
-					});
-				} else {
-					res.render('usuarios', {
-						usuario: req.session.usuario,
-						ver: 'usuarios encontrados'
-					});
-				}
-			});
-		} else {
-			res.render('login', {error: 'Debes iniciar sesión ' +
-				'para acceder a SocialGCap.'});
-		}
-	}); // /usuarios/buscador
-}
+				query.exec(function (err, dataPromocion) {
+					if(err){
+	                  return console.log(err);
+					} else {
+						Usuario.find({"id_promocion": dataPromocion},
+										{_id: 0, fechaNacimiento: 0, email: 0, pass: 0, asignaturasProfesor: 0},
+										function (err, dataUsers) {
+							res.render('usuarios', {usuario: req.session.usuario,
+								promociones: dataPromocion,
+								usuarios: dataUsers,
+								ver: req.params.promocion});
+						});
+					}
+				});
+			} else {
+				res.render('login', {error: 'Debes iniciar sesión ' +
+					'para acceder a SocialGCap.'});
+			}
+
+		}); // /usuarios/promociones
+
+		app.post('/usuarios/buscar', function (req, res) {
+			
+			if (req.session.usuario != undefined) {
+				query = Usuario.find({},{_id: 0, fechaNacimiento: 0, email: 0, pass: 0, asignaturasProfesor: 0})
+								.or([{'usuario': {$regex: new RegExp(req.body.buscador, "i")}}, 
+									{'nombre': {$regex: new RegExp(req.body.buscador, "i")}}, 
+									{'apellidos': { $regex: new RegExp(req.body.buscador, "i")}}])
+								.sort({nombre: 1, apellidos: 1});
+
+				query.exec(function (err, users) {
+					if (err) {
+						return console.log(err);
+					}
+
+					if (users) {
+						res.render('usuarios', {
+							usuario: req.session.usuario,
+							usuarios: users,
+							ver: 'usuarios encontrados'
+						});
+					} else {
+						res.render('usuarios', {
+							usuario: req.session.usuario,
+							ver: 'usuarios encontrados'
+						});
+					}
+				});
+			} else {
+				res.render('login', {error: 'Debes iniciar sesión ' +
+					'para acceder a SocialGCap.'});
+			}
+		}); // /usuarios/buscador
+	}
 
 module.exports = route;
