8f515f8715b0456118f79c7608280bfb023e8ec6
zacr0 <zacro.suarez@gmail.com>
2014-06-08 21:34:52 +0200

    Arregla fallo de sintaxis

--- application/routes/index.js
+++ application/routes/index.js
@@ -19,19 +19,19 @@ var Usuario = require('../models/Usuario'),
                 res.redirect('perfil');
             } else {
                 res.render('login');
-            };
+            }
         }); // app.get/login
 
-        app.post('/login', function(req, res) {
+        app.post('/login', function (req, res) {
             Usuario.findOne({usuario: req.body.usuario, pass: req.body.pass},
-               function(err, user) {
+             function (err, user) {
                 var hash = crypto
                                 .createHash('md5')
                                 .update(req.body.pass)
                                 .digest("hex");
 
                 Usuario.findOne({usuario: req.body.usuario, pass: hash},
-                   function (err, user) {
+                 function (err, user) {
                     if (err) {
                         console.log('Error al buscar usuario en la BD');
                     }
@@ -45,13 +45,15 @@ var Usuario = require('../models/Usuario'),
                         req.session.id_curso = user.id_curso;
 
                         res.redirect('/perfil/' + req.session.usuario);
-                } else {
-                    console.log('El usuario no existe');
-                    res.render('login', {error: 'El usuario introducido no existe. ' +
-                        'Compruebe que la información que ha introducido sea ' +
-                        'correcta e inténtelo de nuevo.'});
-                }
-            });
+                    } else {
+                        console.log('El usuario no existe');
+                        res.render('login', {error: 'El usuario introducido no existe. ' +
+                            'Compruebe que la información que ha introducido sea ' +
+                            'correcta e inténtelo de nuevo.'
+                        });
+                    }
+                });
+            })
         }); // app.post/login
 
         // Registro
@@ -68,7 +70,6 @@ var Usuario = require('../models/Usuario'),
                                 asignaturaData = data;
                                 callback();
                             });
-
                         });
                     });
                 }, function resultados(callback) {
@@ -100,6 +101,7 @@ var Usuario = require('../models/Usuario'),
                     });
 
                     stream.on('close', function () {
+                        console.log('Ok');
                         callback();
                     });
 
@@ -112,7 +114,6 @@ var Usuario = require('../models/Usuario'),
                     user = new Usuario();
                     user.usuario = req.body.usuario;
                     user.pass = hash;
-
                     // Foto por defecto del usuario
                     user.foto = '/img/avatar_default.jpg';
                     user.nombre = req.body.nombre;
@@ -132,75 +133,29 @@ var Usuario = require('../models/Usuario'),
                                     Curso.findOne({nombre: req.body.curso}, function (err, data){
                                         user.id_curso = data.id;
                                         callback();
-                                }); // curso
-                            }); // promocion
-                        } // else
-                    }, function (callback) {
-                        user.save(function (err) {
-                            if (err) {
-                                req.session.error = err;
-                                console.log('Error al registrar usuario');
-                                res.render('registro', {error: req.session.error, 
-                                    cursoData: cursoData, 
-                                    promocionData: promocionData});
-                            }
-                        });
+                                    }); // curso
+                                }); // promocion
+                            } // else
+                        }, function (callback) {
+                            user.save(function (err) {
+                                if (err) {
+                                    req.session.error = err;
+                                    console.log('Error al registrar usuario');
+                                    res.render('registro', {error: req.session.error, 
+                                        cursoData: cursoData, 
+                                        promocionData: promocionData
+                                    });
+                                    return console.error(err);
+                                }
+                                console.log('Usuario registrado');
+                                res.render('login', {success: true});
+                            }); // save
+                        }
+                    ]); // async.series
 
-                        stream.on('error', function (err) {
-                            console.error(err);
-                        });
-
-                        stream.on('close', function () {
-                            console.log('Ok');
-                            callback();
-                        });
-
-                    }, function (callback) {
-                        user = new Usuario();
-                        user.usuario = req.body.usuario;
-                        user.pass = req.body.pass;
-                        // Foto por defecto del usuario
-                        user.foto = '/img/avatar_default.jpg';
-                        user.nombre = req.body.nombre;
-                        user.apellidos = req.body.apellidos;
-                        user.email = req.body.email;
-                        user.fechaNacimiento = req.body.fechanacimiento;
-                        user.perfil = req.body.perfil;
-
-                        async.series([
-                            function (callback){
-                                if (req.body.perfil === 'profesor') {
-                                    user.asignaturasProfesor = req.body.asignatura;
-                                    callback();
-                                } else{
-                                    Promocion.findOne({nombre: req.body.promocion}, function (err, data){
-                                        user.id_promocion = data.id;
-                                        Curso.findOne({nombre: req.body.curso}, function (err, data){
-                                            user.id_curso = data.id;
-                                            callback();
-                                        }); // curso
-                                    }); // promocion
-                                } // else
-                            }, function (callback) {
-                                user.save(function (err) {
-                                    if (err) {
-                                        req.session.error = err;
-                                        console.log('Error al registrar usuario');
-                                        res.render('registro', {error: req.session.error, 
-                                            cursoData: cursoData, 
-                                            promocionData: promocionData
-                                        });
-                                        return console.error(err);
-                                    } else {
-                                        console.log('Usuario registrado');
-                                        res.render('login', {success: true});
-                                    }
-                                }); // save
-                            }
-                        ]); // async.series
                 } // function
             ]); // async.series
-    	}); // app.post/registro
+        }); // app.post/registro
 
         // Logout
         app.get('/logout', function (req, res) {
